c552294f4164c5a58ada7a31ce68d6cbf8770650
Jeroen Cottaar <199474359+jcottaar@users.noreply.github.com>
2025-09-26 21:30:39 +0200

    Path update

--- core/kaggle_support.py
+++ core/kaggle_support.py
@@ -66,7 +66,7 @@ match env:
         data_dir = '/kaggle/input/ariel-data-challenge-2025/'
         temp_dir = '/temp/'           
         loader_cache_dir = '/temp/loader_cache/'
-        code_dir = '/kaggle/input/my-ariel2-library/'         
+        code_dir = '/kaggle/input/my-ariel-library-3/'         
         csv_dir = '/kaggle/working/'
         calibration_dir = '/kaggle/input/my-ariel2-calibration/'
 os.makedirs(temp_dir, exist_ok=True)
